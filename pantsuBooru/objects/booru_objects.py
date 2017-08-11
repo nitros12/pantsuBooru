82e0e43bba79179dd01857ecb7dfe31816fa36d9
ben simms <ben@bensimms.moe>
2017-08-11 02:51:46 +0100

    more work on booru objects

--- pantsuBooru/objects/booru_objects.py
+++ pantsuBooru/objects/booru_objects.py
@@ -1,24 +1,67 @@
 from pantsuBooru.backend import BooruDatabase
-from pantsuBooru.models import Image, Tag, User
-from pantsuBooru.objects.user import BooruUser
+from pantsuBooru.models import Image, Tag, User, Comment, ImageTag
+import inspect
 
 
-class BooruImage:
-    def __init__(self, db: BooruDatabase, row: Image):
+class AsyncMeta(type):
+    async def __call__(self, *args, **kwargs):
+        obb = object.__new__(self)
+        fn = obb.__init__(*args, **kwargs)
+        if inspect.isawaitable(fn):
+            await fn
+        return obb
+
+class BooruBase(metaclass=AsyncMeta):
+    async def __init__(self, db: BooruDatabase, row: 'Table'):
         self.db = db
         self.row = row
 
-    async def add_tags(self, *tags: str) -> [Tag]:
-        """Add tags to the image.
+    def __getattr__(self, attr):
+        try:
+            return super().__getattr__(attr)
+        except AttributeError as e:
+            pass
 
-        :param tags: The tags to add to this image.
+        return getattr(self.row, attr)
+
+    def to_json():
+        raise NotImplemented
+
+
+class BooruImage(BooruBase):
+    async def __init__(self, db: BooruDatabase, row: Image):
+        await super().__init__(db, row)
+        self.poster = await self._get_poster()
+        self.tags = list(row.tags)
+        self.comments = list(row.comments)
+
+    async def replace_tags(self, *tags: str) -> [Tag]:
+        """Replace tags on the image.
+
+        :param tags: The tags to replace on the image.
         """
-        return await self.db.add_tags(*tags)
+        return await self.db.replace_tags(self.id, *tags)
 
-    async def get_poster(self) -> 'BooruUser':
+    async def _get_poster(self) -> 'BooruUser':
         """Get the poster of the image."""
-        return await self.db.get_user(id=self.row.poster)
+        return await self.db.get_user(id=self.poster)
+
+    async def add_comment(self, text: str, poster: 'BooruUser'):
+        comment = Comment(text=text, poster=poster)
+        async with self.db.get_session() as s:
+            self.comments.append(await self.comments.add(comment))
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "posted_at": self.posted_at.timestamp(),
+            "author": self.author,
+            "source": self.source,
+            "poster": self.poster.to_json(),
+            "tags": [i.to_json() for i in self.tags],
+            "comments": [i.to_json() for i in self.comments]
+        }
 
 
-class BooruUser:
+class BooruUser(BooruBase):
     # TODO: Fill in BooruUser methods
